3a0a22203d2ef994d6c70d988e16aae630f20999
Matthew Austin Miller <mattmiller899@login2.cm.cluster>
2019-01-31 08:32:37 -0700

    passing all test cases

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -2,16 +2,19 @@
 
 DIR="$PWD/../../data/gapminder/"
 FILE_LIST="$PWD/file_list"
-ls $DIR | sort > $FILE_LIST
+if [[ $# -eq 0 ]]; then
+    ls $DIR | sort > $FILE_LIST
+elif [[ $# -eq 1 ]]; then
+    ls $DIR | grep -ie "^$1" | sort > $FILE_LIST
+fi
 
 COUNT=1
-if [[ $# -eq 0 ]]; then
+LC=$(wc -l $FILE_LIST | awk '{print $1}')
+if [[ $LC -eq 0 ]]; then
+    echo "There are no countries starting with \"$1\""
+else
     while read -r LINE; do
         echo "$COUNT $(basename $LINE .cc.txt)"
         COUNT=$((COUNT + 1))
-    done < $FILE_LIST
-else
-    while read -r LINE; do
-        basename $LINE .cc.txt
     done < $FILE_LIST
 fi
