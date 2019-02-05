b406827a044bf0de23191b7b2c4a37079a43d809
Matthew Austin Miller <mattmiller899@login2.cm.cluster>
2019-02-05 10:41:16 -0700

    passing all test cases

---
+++ assignments/03-python-grad/grid.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: grid.py NUM")
+    exit(1)
+num = int(sys.argv[1])
+if num < 2 or num > 9:
+    print("NUM ({}) must be between 1 and 9".format(num))
+    exit(1)
+print_str = ""
+curr_num = 1
+for i in range(num):
+    print_str = "{}{}".format(
+                            print_str,
+                            "  " if curr_num < 9 else " ")
+    for j in range(1, num+1):
+        curr_num = j+(i*num)
+        split_char = ""
+        if j == num:
+            split_char = "\n"
+        elif curr_num < 9:
+            split_char = "  "
+        else:
+            split_char = " "
+        print_str = "{}{}{}".format(
+                                    print_str,
+                                    curr_num,
+                                    split_char)
+    #print_str = "{}\n".format(print_str)
+print(print_str[:-1])
