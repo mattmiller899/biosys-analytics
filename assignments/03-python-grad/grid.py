9480122376bd71e0a7a28bf6d0ef5be4a07f7f16
Matthew Austin Miller <mattmiller899@login2.cm.cluster>
2019-02-05 10:45:23 -0700

    new passing

--- assignments/03-python-grad/grid.py
+++ assignments/03-python-grad/grid.py
@@ -12,21 +12,25 @@ if num < 2 or num > 9:
 print_str = ""
 curr_num = 1
 for i in range(num):
-    print_str = "{}{}".format(
-                            print_str,
-                            "  " if curr_num < 9 else " ")
+    #print_str = "{}{}".format(
+    #                        print_str,
+    #                        "  " if curr_num < 9 else " ")
     for j in range(1, num+1):
-        curr_num = j+(i*num)
-        split_char = ""
-        if j == num:
-            split_char = "\n"
-        elif curr_num < 9:
-            split_char = "  "
-        else:
-            split_char = " "
-        print_str = "{}{}{}".format(
-                                    print_str,
-                                    curr_num,
-                                    split_char)
+        print("{:3}".format(j+i*num), end='')
+    print()
+    """
+    curr_num = j+(i*num)
+    split_char = ""
+    if j == num:
+        split_char = "\n"
+    elif curr_num < 9:
+        split_char = "  "
+    else:
+        split_char = " "
+    print_str = "{}{}{}".format(
+                                print_str,
+                                curr_num,
+                                split_char)
+    """
     #print_str = "{}\n".format(print_str)
-print(print_str[:-1])
+#print(print_str[:-1])
